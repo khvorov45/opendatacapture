c7a2aceabe2f5baa48566e99d0adb37756d060e4
khvorov45 <khvorov45@gmail.com>
2020-07-11 17:11:35 +1000

    add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Intended use
+
+- Install Postgres, create the default user and database for this api to
+connect to.
+
+    - Write down the Postgres connection details in a config file the path
+    to which will be passed to the api via a command-line argument at launch.
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello =
-        warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
-
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    let routes = warp::any().map(|| "Hello, World!");
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
