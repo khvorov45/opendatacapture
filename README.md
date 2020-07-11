1d01ec4c2de3b4c4f1c21346674b5ff17622dfea
khvorov45 <khvorov45@gmail.com>
2020-07-11 17:58:59 +1000

    postgres setup

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 - Install Postgres, create the default user and database for this api to
 connect to.
 
-    - Write down the Postgres connection details in a config file the path
-    to which will be passed to the api via a command-line argument at launch.
+    - Postgres connection details can be passed to the api via command-line
+    arguments at launch. The defaults are:
+        - host: localhost
+        - user: odcdefault
+        - password: odcdefault
+        - port: 5432
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
+use tokio_postgres::{Error, NoTls};
 use warp::Filter;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
+    let mut dbconfig = tokio_postgres::config::Config::new();
+    dbconfig
+        .host("localhost")
+        .user("odcdefault")
+        .password("odcdefault")
+        .port(5432);
+    // Connect to the database.
+    let (client, connection) = dbconfig.connect(NoTls).await?;
+
+    // The connection object performs the actual communication with
+    // the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    // Now we can execute a simple statement that just returns its parameter.
+    let rows = client.query("SELECT $1::TEXT", &[&"hello world"]).await?;
+    println!("rows: {:?}", rows);
     let routes = warp::any().map(|| "Hello, World!");
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    Ok(())
 }
