3a345882bdbf3461aa4d97681af29573b4d74267
Arseniy Khvorov <khvorov45@gmail.com>
2020-07-17 13:36:49 +1000

    moved execution to run

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use structopt::StructOpt;
+use tokio_postgres::{Error, NoTls};
+use warp::Filter;
 
 /// opendatacapture
 #[derive(StructOpt, Debug)]
@@ -8,7 +10,7 @@ pub struct Opt {
     pub host: String,
     /// Host port
     #[structopt(long, default_value = "5432")]
-    pub port: i32,
+    pub port: u16,
     /// Postgres database name to use as default
     #[structopt(long, default_value = "odcdefault")]
     pub dbname: String,
@@ -20,6 +22,27 @@ pub struct Opt {
     pub password: String,
 }
 
-pub fn run(opt: Opt) {
-    println!("{:?}", opt)
+pub async fn run(opt: Opt) -> Result<(), Error> {
+    let mut dbconfig = tokio_postgres::config::Config::new();
+    dbconfig
+        .host(opt.host.as_str())
+        .port(opt.port)
+        .dbname(opt.dbname.as_str())
+        .user(opt.user.as_str())
+        .password(opt.password);
+    // Connect to the database.
+    let (client, connection) = dbconfig.connect(NoTls).await?;
+    // The connection object performs the actual communication with
+    // the database, so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+    // Now we can execute a simple statement that just returns its parameter.
+    let rows = client.query("SELECT $1::TEXT", &[&"hello world"]).await?;
+    println!("rows: {:?}", rows);
+    let routes = warp::any().map(|| "Hello, World!");
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,9 @@
 use opendatacapture::{run, Opt};
 use structopt::StructOpt;
-use tokio_postgres::{Error, NoTls};
-use warp::Filter;
+use tokio_postgres::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let opt = Opt::from_args();
-    run(opt);
-
-    let mut dbconfig = tokio_postgres::config::Config::new();
-    dbconfig
-        .host("localhost")
-        .port(5432)
-        .dbname("odcdefault")
-        .user("odcdefault")
-        .password("odcdefault");
-    // Connect to the database.
-    let (client, connection) = dbconfig.connect(NoTls).await?;
-
-    // The connection object performs the actual communication with
-    // the database,
-    // so spawn it off to run on its own.
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
-
-    // Now we can execute a simple statement that just returns its parameter.
-    let rows = client.query("SELECT $1::TEXT", &[&"hello world"]).await?;
-    println!("rows: {:?}", rows);
-    let routes = warp::any().map(|| "Hello, World!");
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
-
-    Ok(())
+    run(opt).await
 }
