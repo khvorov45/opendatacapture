c2e65fbe06db284de8ad805b488ad3ac2f734069
Arseniy Khvorov <khvorov45@gmail.com>
2020-07-24 11:01:44 +1000

    took default admin insertion out to a function

--- src/lib.rs
+++ src/lib.rs
@@ -57,43 +57,21 @@ pub async fn run(opt: Opt) -> Result<(), Box<dyn Error>> {
     let admindb =
         db::DB::new("admin", &dbconfig, get_admin_tablespec(), !opt.clean)
             .await?;
-    // Insert an admin if empty
-    if admindb.get_rows_json("admin").await?.is_empty() {
-        log::info!(
-            "no admins found, inserting \"{}\" with password \"{}\"",
-            opt.admin_email,
-            opt.admin_password
-        );
-        let admin_password_hash = argon2::hash_encoded(
-            opt.admin_password.as_bytes(),
-            gen_rand_string().as_bytes(),
-            &argon2::Config::default(),
-        )?;
-        admindb
-            .insert(&db::table::TableJson::new(
-                "admin",
-                vec![serde_json::from_str(
-                    format!(
-                        "{{\"email\": \"{}\", \"password_hash\": \"{}\"}}",
-                        opt.admin_email, admin_password_hash
-                    )
-                    .as_str(),
-                )?],
-            ))
-            .await?;
-    }
-
+    insert_admin_if_empty(&admindb, opt.admin_email, opt.admin_password)
+        .await?;
     let routes = warp::any().map(|| "Hello, World!");
     warp::serve(routes).run(([127, 0, 0, 1], opt.apiport)).await;
     Ok(())
 }
 
+/// Tables for the admin database
 fn get_admin_tablespec() -> db::TableSpec {
     let mut set = db::TableSpec::new();
     set.push(db::TableMeta::new("admin", get_admin_colspec(), ""));
     set
 }
 
+/// Columns for the admin table
 fn get_admin_colspec() -> db::ColSpec {
     let mut set = db::ColSpec::new();
     set.push(db::ColMeta::new("id", "SERIAL", "PRIMARY KEY"));
@@ -102,10 +80,43 @@ fn get_admin_colspec() -> db::ColSpec {
     set
 }
 
+/// Generates a random string
 fn gen_rand_string() -> String {
     use rand::Rng;
     rand::thread_rng()
         .sample_iter(&rand::distributions::Alphanumeric)
         .take(30)
         .collect()
+}
+
+/// Insert an admin if the admin table is empty
+async fn insert_admin_if_empty(
+    admindb: &db::DB,
+    admin_email: String,
+    admin_password: String,
+) -> Result<(), Box<dyn Error>> {
+    if !admindb.get_rows_json("admin").await?.is_empty() {
+        return Ok(());
+    }
+    log::info!(
+        "no admins found, inserting \"{}\" with password \"{}\"",
+        admin_email,
+        admin_password
+    );
+    let admin_password_hash = argon2::hash_encoded(
+        admin_password.as_bytes(),
+        gen_rand_string().as_bytes(),
+        &argon2::Config::default(),
+    )?;
+    let admin_json = format!(
+        "{{\"email\": \"{}\", \"password_hash\": \"{}\"}}",
+        admin_email, admin_password_hash
+    );
+    admindb
+        .insert(&db::table::TableJson::new(
+            "admin",
+            vec![serde_json::from_str(admin_json.as_str())?],
+        ))
+        .await?;
+    Ok(())
 }
