bdac16209378fbea54b8d62dbb75eb9c6ad042c1
Arseniy Khvorov <khvorov45@gmail.com>
2020-08-06 12:30:38 +1000

    add cssbaseline

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { createMuiTheme, ThemeProvider, Theme } from "@material-ui/core/styles"
 import Nav from "./components/nav"
 import Login from "./components/login"
 import { IdToken } from "./lib/auth"
+import CssBaseline from "@material-ui/core/CssBaseline"
 
 function createThemeFromPalette(palette: "dark" | "light"): Theme {
   return createMuiTheme({
@@ -32,6 +33,7 @@ export default function App({
   }
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Nav handleThemeChange={handleThemeChange} />
       <Login updateCred={updateCred} />
     </ThemeProvider>
