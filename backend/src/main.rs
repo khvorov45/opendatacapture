607cd6acb457caf9150588f9748f2e17994c5189
khvorov45 <khvorov45@gmail.com>
2020-08-08 16:04:41 +1000

    ignore main for coverage

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,6 +2,7 @@ use anyhow::{Context, Result};
 use opendatacapture::{run, Opt};
 use structopt::StructOpt;
 
+#[cfg(not(tarpaulin_include))]
 #[tokio::main]
 async fn main() -> Result<()> {
     pretty_env_logger::init();
