8c0bf561b3980ece0d20016f971c3b147ad16e29
khvorov45 <khvorov45@gmail.com>
2020-08-29 14:40:25 +1000

    frame for project widget

--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -1,28 +1,122 @@
 import React, { useEffect, useState } from "react"
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  useTheme,
+} from "@material-ui/core/styles"
+import Add from "@material-ui/icons/Add"
 import { getUserProjects, Project } from "../lib/project"
 
 export default function Home({ token }: { token: string | null }) {
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      homePage: {
+        display: "flex",
+        justifyContent: "center",
+      },
+    })
+  )
+  const classes = useStyles()
   return (
-    <div data-testid="homepage">
-      {token ? <ProjectList token={token} /> : "Loading"}
+    <div className={classes.homePage} data-testid="homepage">
+      {token ? <ProjectWidget token={token} /> : "Loading"}
+    </div>
+  )
+}
+
+function ProjectWidget({ token }: { token: string }) {
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      projectWidget: {
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        marginTop: "20px",
+      },
+    })
+  )
+  const classes = useStyles()
+  return (
+    <div className={classes.projectWidget} data-testid="projectWidget">
+      <ProjectControl token={token} />
+      <ProjectList token={token} />
+    </div>
+  )
+}
+
+function ProjectControl({ token }: { token: string }) {
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      projectControl: {
+        display: "flex",
+        alignItems: "center",
+      },
+    })
+  )
+  const classes = useStyles()
+  return (
+    <div className={classes.projectControl} data-testid="projectControl">
+      <ProjectCreate token={token} />
     </div>
   )
 }
 
 function ProjectList({ token }: { token: string }) {
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      projectList: {
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+      },
+    })
+  )
+  const classes = useStyles()
   let [projects, setProjects] = useState<Project[]>([])
   useEffect(() => {
     getUserProjects(token).then((ps) => setProjects(ps))
   }, [token])
   return (
-    <div data-testid="project-list">
-      {projects.length
-        ? projects.map((p) => <ProjectEntry project={p} />)
-        : "No projects"}
+    <div className={classes.projectList} data-testid="project-list">
+      {projects.length ? (
+        projects.map((p) => <ProjectEntry project={p} token={token} />)
+      ) : (
+        <NoProjects token={token} />
+      )}
     </div>
   )
 }
 
-function ProjectEntry({ project }: { project: Project }) {
+function ProjectEntry({ project, token }: { project: Project; token: string }) {
   return <div>Project entry {JSON.stringify(project)}</div>
+}
+
+function NoProjects({ token }: { token: string }) {
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      noProjects: {
+        display: "flex",
+        alignItems: "center",
+      },
+    })
+  )
+  const classes = useStyles()
+  return (
+    <div className={classes.noProjects} data-testid="no-projects">
+      <p>Click</p> <ProjectCreate token={token} /> <p>to create a project</p>
+    </div>
+  )
+}
+
+function ProjectCreate({ token }: { token: string }) {
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      create: {
+        cursor: "pointer",
+      },
+    })
+  )
+  const classes = useStyles()
+  return <Add className={classes.create} />
 }
