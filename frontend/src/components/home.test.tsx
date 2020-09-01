7d85dec6b27dc78d9f21b31ec4ffe7f7007fc7b4
Arseniy Khvorov <khvorov45@gmail.com>
2020-09-01 11:32:05 +1000

    homepage with no token

--- frontend/src/components/home.test.tsx
+++ frontend/src/components/home.test.tsx
@@ -11,6 +11,11 @@ function renderHome(token: string | null) {
   return render(<Home token={token} />)
 }
 
+test("homepage with no token", () => {
+  const { queryByTestId } = renderHome(null)
+  expect(queryByTestId("project-widget")).not.toBeInTheDocument()
+})
+
 test("homepage with token and no projects", async () => {
   // Get projects
   mockedAxios.get.mockResolvedValueOnce({ data: [] })
@@ -20,7 +25,7 @@ test("homepage with token and no projects", async () => {
   expect(getByTestId("project-widget")).toBeInTheDocument()
   // Check that the new project from is visible
   expect(getByTestId("project-create-form")).not.toHaveClass(
-    "makeStyles-hidden-7"
+    "makeStyles-hidden-8"
   )
 })
 
@@ -34,5 +39,5 @@ test("homepage with token and some projects", async () => {
   expect(getByTestId("homepage")).toBeInTheDocument()
   expect(getByTestId("project-widget")).toBeInTheDocument()
   // Check that the new project form is hidden
-  expect(getByTestId("project-create-form")).toHaveClass("makeStyles-hidden-16")
+  expect(getByTestId("project-create-form")).toHaveClass("makeStyles-hidden-17")
 })
