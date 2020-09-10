6bd956de79f3577cd317ff6862b729c5937d446f
Arseniy Khvorov <khvorov45@gmail.com>
2020-09-11 09:30:31 +1000

    type useParams

--- frontend/src/components/project.tsx
+++ frontend/src/components/project.tsx
@@ -2,6 +2,6 @@ import React from "react"
 import { useParams } from "react-router-dom"
 
 export default function ProjectPage() {
-  let { name } = useParams()
+  let { name } = useParams<{ name: string }>()
   return <div>Project page for {name}</div>
 }
