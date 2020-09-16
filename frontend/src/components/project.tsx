a9593aa16b6a2d888515873e7d92b23a95301ca7
Arseniy Khvorov <khvorov45@gmail.com>
2020-09-16 12:25:26 +1000

    sidebar links

--- frontend/src/components/project.tsx
+++ frontend/src/components/project.tsx
@@ -5,9 +5,10 @@ import {
   ListItemText,
   Theme,
 } from "@material-ui/core"
+import { Link, Redirect } from "react-router-dom"
 import makeStyles from "@material-ui/core/styles/makeStyles"
 import React, { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { Route, useParams } from "react-router-dom"
 import { TableMeta, TableSpec } from "../lib/project"
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -40,21 +41,27 @@ export default function ProjectPage({
   const classes = useStyles()
   return (
     <div className={classes.projectPage} data-testid={`project-page-${name}`}>
-      <Sidebar />
+      <Sidebar projectName={name} />
       <main className={classes.main}>
         <div>main</div>
-        <TableCards tableSpec={tableSpec} />
+        <Route path={`/project/${name}`}>
+          <Redirect to={`/project/${name}/tables`} />
+        </Route>
+        <Route path={`/project/${name}/tables`}>
+          <div>tables</div>
+          <TableCards tableSpec={tableSpec} />
+        </Route>
       </main>
     </div>
   )
 }
 
-function Sidebar() {
+function Sidebar({ projectName }: { projectName: string }) {
   const classes = useStyles()
   return (
     <div className={classes.sidebar}>
       <List>
-        <ListItem button>
+        <ListItem button component={Link} to={`/project/${projectName}/tables`}>
           <ListItemText primary="Tables" />
         </ListItem>
       </List>
