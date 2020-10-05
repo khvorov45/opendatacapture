be7b205003d5b1512da84546a09fb64ed1d924e3
Arseniy Khvorov <khvorov45@gmail.com>
2020-10-06 07:12:03 +1100

    add control/nav to data

--- frontend/src/components/project/data-panel.tsx
+++ frontend/src/components/project/data-panel.tsx
@@ -1,6 +1,24 @@
+import { makeStyles, Theme, createStyles } from "@material-ui/core"
 import React, { useCallback, useEffect, useState } from "react"
-import { trackPromise } from "react-promise-tracker"
+import { trackPromise, usePromiseTracker } from "react-promise-tracker"
 import { getAllTableNames } from "../../lib/api/project"
+import { ButtonArray, RefreshButton } from "../button"
+import { SimpleNav } from "../nav"
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    dataControl: {
+      display: "flex",
+      borderBottom: `1px solid ${theme.palette.divider}`,
+      "&>*:nth-child(odd)": {
+        borderRight: `1px solid ${theme.palette.divider}`,
+      },
+      "& *": {
+        height: 48,
+      },
+    },
+  })
+)
 
 export default function DataPanel({
   token,
@@ -11,8 +29,9 @@ export default function DataPanel({
 }) {
   const [tableNames, setTableNames] = useState<string[] | null>(null)
   const [errorMsg, setErrorMsg] = useState("")
+  const { promiseInProgress } = usePromiseTracker({ area: "getAllTableNames" })
   const refreshTables = useCallback(() => {
-    trackPromise(getAllTableNames(token, projectName), "getAllMeta")
+    trackPromise(getAllTableNames(token, projectName), "getAllTableNames")
       .then((tables) => {
         setErrorMsg("")
         setTableNames(tables)
@@ -22,8 +41,20 @@ export default function DataPanel({
   useEffect(() => {
     refreshTables()
   }, [refreshTables])
+  const classes = useStyles()
   return (
     <div data-testid="data-panel">
+      <div className={classes.dataControl}>
+        <ButtonArray errorMsg={errorMsg} errorTestId="refresh-tables-error">
+          <RefreshButton
+            onClick={refreshTables}
+            inProgress={promiseInProgress}
+            dataTestId="refresh-tables-button"
+          />
+        </ButtonArray>
+        <SimpleNav links={tableNames ?? []} />
+      </div>
+
       {tableNames === null ? (
         <></>
       ) : tableNames.length === 0 ? (
