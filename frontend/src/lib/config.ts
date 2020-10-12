566eb64407047be46e169a7f20a9cd04bc50964a
Arseniy Khvorov <khvorov45@gmail.com>
2020-10-13 09:47:39 +1100

    actually parse the env var

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,5 +1,6 @@
 export const API_ROOT =
   process.env.REACT_APP_ODC_API_ROOT || "http://localhost:4321"
 
-export const TOKEN_HOURS_TO_REFRESH =
-  process.env.REACT_APP_TOKEN_HOURS_TO_REFRESH || 12
+export const TOKEN_HOURS_TO_REFRESH = parseInt(
+  process.env.REACT_APP_TOKEN_HOURS_TO_REFRESH || "12"
+)
