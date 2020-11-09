b1edca4007e5f18867c7afcebb2089c74ee55f45
Arseniy Khvorov <khvorov45@gmail.com>
2020-11-09 12:28:56 +1100

    move renderAdminPage into tests/util

--- frontend/src/components/admin/admin.test.tsx
+++ frontend/src/components/admin/admin.test.tsx
@@ -1,16 +1,8 @@
 /* istanbul ignore file */
 import axios from "axios"
-import {
-  fireEvent,
-  render,
-  wait,
-  waitForDomChange,
-  within,
-} from "@testing-library/react"
+import { fireEvent, wait } from "@testing-library/react"
 import httpStatusCodes from "http-status-codes"
-import React from "react"
-import { MemoryRouter, Route, Redirect, Switch } from "react-router-dom"
-import AdminDashboard from "./admin"
+import { renderAdminPage } from "../../tests/util"
 
 jest.mock("axios")
 const mockedAxios = axios as jest.Mocked<typeof axios>
@@ -19,27 +11,8 @@ mockedAxios.get.mockImplementation(async (url) => {
   return { status: httpStatusCodes.OK, data: [] }
 })
 
-function renderAdmin(token?: string, path?: string) {
-  let tok: string | null = "123"
-  if (token !== undefined) {
-    tok = token
-  }
-  return render(
-    <MemoryRouter initialEntries={[path ? `/admin/${path}` : "/"]}>
-      <Switch>
-        <Route exact path="/">
-          <Redirect to="/admin" />
-        </Route>
-        <Route path="/admin">
-          <AdminDashboard token={tok} />
-        </Route>
-      </Switch>
-    </MemoryRouter>
-  )
-}
-
 test("navigation", async () => {
-  const admin = renderAdmin()
+  const admin = renderAdminPage()
   await wait(() =>
     expect(admin.getByTestId("users-admin-widget")).toBeInTheDocument()
   )
