b226358be24ee5aab9ea57dbfa2468734dbfb965
Arseniy Khvorov <khvorov45@gmail.com>
2020-11-12 08:45:55 +1100

    test user refresh

--- frontend/src/components/admin/users.test.tsx
+++ frontend/src/components/admin/users.test.tsx
@@ -1,12 +1,17 @@
 /* istanbul ignore file */
 import axios from "axios"
-import { fireEvent, render, wait } from "@testing-library/react"
+import {
+  fireEvent,
+  render,
+  wait,
+  waitForDomChange,
+} from "@testing-library/react"
 import httpStatusCodes from "http-status-codes"
-import { renderAdminPage } from "../../tests/util"
+import { defaultAdmin } from "../../tests/util"
 import { constructGet } from "../../tests/api"
+import { user1 } from "../../tests/data"
 import React from "react"
 import Users from "./users"
-import { act } from "react-dom/test-utils"
 
 jest.mock("axios")
 const mockedAxios = axios as jest.Mocked<typeof axios>
@@ -29,4 +34,30 @@ test("new user form open/close", async () => {
   expect(frm).not.toHaveClass("nodisplay")
   fireEvent.click(btn)
   expect(frm).toHaveClass("nodisplay")
+})
+
+test("refresh users", async () => {
+  const users = renderUsers()
+  await wait(() => {
+    expect(users.getByTestId("users-admin-widget")).toBeInTheDocument()
+  })
+  // Only admin should be in the table
+  expect(users.getByText(defaultAdmin.email)).toBeInTheDocument()
+  expect(users.queryByText(user1.email)).not.toBeInTheDocument()
+
+  // Refresh
+  mockedAxios.get.mockImplementationOnce(
+    constructGet({
+      getUsers: async () => ({
+        status: httpStatusCodes.OK,
+        data: [defaultAdmin, user1],
+      }),
+    })
+  )
+  fireEvent.click(users.getByTestId("refresh-users-button"))
+  await waitForDomChange()
+
+  // Both user and admin should be in the table
+  expect(users.getByText(defaultAdmin.email)).toBeInTheDocument()
+  expect(users.getByText(user1.email)).toBeInTheDocument()
 })
