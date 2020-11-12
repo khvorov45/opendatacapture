4a70b662445d08d117f423a778d0a442b8522b38
Arseniy Khvorov <khvorov45@gmail.com>
2020-11-12 11:48:20 +1100

    test create error

---
+++ frontend/src/lib/api/user.test.ts
@@ -0,0 +1,25 @@
+// Test whatever can't be tested in fullstack
+/* istanbul ignore file */
+
+import axios from "axios"
+import httpStatusCodes from "http-status-codes"
+import { constructPut } from "../../tests/api"
+import { newUser } from "../../tests/util"
+import { createUser } from "./user"
+jest.mock("axios")
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+mockedAxios.put.mockImplementation(constructPut())
+
+test("createUser", async () => {
+  mockedAxios.put.mockResolvedValueOnce({
+    status: httpStatusCodes.INTERNAL_SERVER_ERROR,
+    data: "some create user error",
+  })
+  expect.assertions(1)
+  try {
+    await createUser(newUser)
+  } catch (e) {
+    expect(e.message).toBe("some create user error")
+  }
+})
