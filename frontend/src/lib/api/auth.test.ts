cd35b110cac3dfe5bb6639a4977eddde1b0eccc8
Arseniy Khvorov <khvorov45@gmail.com>
2020-11-26 11:53:10 +1100

    remove unnecessary mocks

--- frontend/src/lib/api/auth.test.ts
+++ frontend/src/lib/api/auth.test.ts
@@ -1,28 +1,12 @@
 // Test whatever can't be tested in fullstack
 /* istanbul ignore file */
 import httpStatusCodes from "http-status-codes"
-import { fetchToken, validateToken } from "./auth"
+import { fetchToken } from "./auth"
 
 import axios from "axios"
-import {
-  constructGet,
-  constructPut,
-  constructDelete,
-  constructPost,
-} from "../../tests/api"
+import { constructPost } from "../../tests/api"
 jest.mock("axios")
 const mockedAxios = axios as jest.Mocked<typeof axios>
-
-const getreq = mockedAxios.get.mockImplementation(constructGet())
-const putreq = mockedAxios.put.mockImplementation(constructPut())
-const deletereq = mockedAxios.delete.mockImplementation(constructDelete())
-const postreq = mockedAxios.post.mockImplementation(constructPost())
-afterEach(() => {
-  mockedAxios.get.mockImplementation(constructGet())
-  mockedAxios.put.mockImplementation(constructPut())
-  mockedAxios.delete.mockImplementation(constructDelete())
-  mockedAxios.post.mockImplementation(constructPost())
-})
 
 test("fetchToken", async () => {
   expect.assertions(2)
