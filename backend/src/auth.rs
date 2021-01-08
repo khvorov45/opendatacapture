5e14ae4c638c6d2a35a35ec0c5415ba7749e505f
Arseniy Khvorov <khvorov45@gmail.com>
2021-01-09 08:48:49 +1100

    update backend dependencies

--- backend/src/auth.rs
+++ backend/src/auth.rs
@@ -30,6 +30,7 @@ fn gen_rand_string(len: usize) -> String {
     rand::thread_rng()
         .sample_iter(&rand::distributions::Alphanumeric)
         .take(len)
+        .map(|x| x as char)
         .collect()
 }
 
